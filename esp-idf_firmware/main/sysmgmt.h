d988c9fd98a0210169c4d64d03fc0415abe18a34
Kilian Gosewisch <kilian2798@gmail.com>
2025-03-01 02:37:32 +0100

    disable power button

--- esp-idf_firmware/main/sysmgmt.h
+++ esp-idf_firmware/main/sysmgmt.h
@@ -1,4 +1,5 @@
-void initSysmgmt(uint8_t * mode, uint8_t * initaudiodevice);
+#include <stdint.h>
+void initSysmgmt(uint8_t *mode, uint8_t *initaudiodevice);
 void PowerUpMA120x0();
 void SetBluetoothReady();
 void SetBluetoothConnected();
